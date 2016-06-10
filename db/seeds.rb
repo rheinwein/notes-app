ff69b1f35aa7303a47c737b91dd59386b288f9e2
Laura Frank <ljfrank@gmail.com>
2016-06-10 13:56:35 +0200

    Let's assume only I'll use this #foreveralone

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,5 @@
 u = User.create(name: 'Laura Frank', chip_id: 'password', admin: true)
-User.create(name: 'Donald Duck', chip_id: 'ddee2e4e4e1b')
-User.create(name: 'Scrooge McDuck', chip_id: '31a8fb2dcf6b')
-
-# categories = %w(Engineering Marketing Support Sales Personal)
-# categories.map! do |category|
-#   Category.create(name: category)
-# end
+User.create(name: 'Pu der Bär', chip_id: 'ddee2e4e4e1b')
 
 Issue.create(text: 'Make this repository public', user: u)
 Issue.create(text: 'Eat some bacon', user: u)
